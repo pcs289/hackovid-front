913bc9fb090cf889f5e4ad84b3324677ca3ec203
Guillem Moreso <guillem.moreso@gmail.com>
2020-04-08 20:41:24 +0200

    login signup i 404 updated

--- src/views/auth/ErrorPage.js
+++ src/views/auth/ErrorPage.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
 
 function ErrorPage() {
   return (
-    <div>
+    <div id="div-404">
       <h1>ERROR 404 </h1>
+      <img id="sorry-img" src="../../images/sorry.svg" alt="location"></img>
     </div>
   );
 }
