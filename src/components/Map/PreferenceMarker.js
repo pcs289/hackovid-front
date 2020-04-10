8a8b1a5b1dbede31b6826a4f5f446656e3fad1f6
paudelacuesta <paudelacuesta@ciclion.com>
2020-04-10 17:33:54 +0200

    Fix premerge

--- src/components/Map/PreferenceMarker.js
+++ src/components/Map/PreferenceMarker.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Marker, Popup } from 'react-map-gl';
 import LocationIcon from './LocationIcon';
+import AvatarImage from "../AvatarImage";
 
 class PreferenceMarker extends Component {
     state = {
@@ -25,17 +26,10 @@ class PreferenceMarker extends Component {
                 </div>
                 {showPopup ? (
                     <Popup key={`popup-${neighbor._id}`} latitude={latitude} longitude={longitude} closeButton={false}>
-                        <div style={{display: 'flex', flexFlow: 'row nowrap', justifyContent: 'space-around', alignItems: 'center'}}>
-                            <img width="35px" height="35px" src={neighbor.avatarImg} alt={neighbor.username + ' avatar image'} />
+                        <div className="marker">
+                            <AvatarImage avatarImg={neighbor.avatarImg} />
                             <span>{neighbor.username}</span>
                         </div>
-                        <ul>
-                            {neighbor.preferences.map((pref, i) => {
-                                return <li key={i}>
-                                    {pref.type}
-                                </li>
-                            })}
-                        </ul>
                     </Popup>
                 ) : null}
             </>
