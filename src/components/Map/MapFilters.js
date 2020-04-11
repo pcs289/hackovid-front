79e1ed86215815297e77dc907bbe95a0b0a75fae
paudelacuesta <paudelacuesta@ciclion.com>
2020-04-11 20:43:20 +0200

    MapFilter URL query param strings

--- src/components/Map/MapFilters.js
+++ src/components/Map/MapFilters.js
@@ -13,17 +13,39 @@ class MapFilters extends Component {
     radiusChange$ = new Subject().pipe(debounceTime(300));
     radiusSubscription = null;
 
-    componentDidMount() {
-        this.setState({
-            radius: 1000,
-            dayOfWeek: 1,
+    async componentDidMount() {
+        const {radius, dayOfWeek} = this.checkQueryFilter();
+        await this.setState({
+            radius: radius || 1000,
+            dayOfWeek: dayOfWeek || 1,
             dayOfWeekNames: ["dilluns", "dimarts", "dimecres", "dijous", "divendres", "dissabte", "diumenge"]
         });
 
+        if (radius && dayOfWeek) {
+            this.props.onFiltersChange({ radius: this.state.radius, dayOfWeek: this.state.dayOfWeek });
+        }
+
         this.radiusSubscription = this.radiusChange$.subscribe(async (debounced) => {
-            await this.setState({ radius: debounced });
+            this.addQuery('r', debounced);
             this.props.onFiltersChange({ radius: this.state.radius, dayOfWeek: this.state.dayOfWeek });
         })
+    }
+
+    checkQueryFilter() {
+        const searchParams = new URLSearchParams(this.props.location.search);
+        const radius = parseInt(searchParams.get('r')) || null;
+        const dayOfWeek = parseInt(searchParams.get('dow')) || null;
+        return { radius, dayOfWeek };
+    }
+
+    addQuery(key, value) {
+        let pathname = this.props.location.pathname;
+        let searchParams = new URLSearchParams(this.props.location.search);
+        searchParams.set(key, value);
+        this.props.history.push({
+            pathname: pathname,
+            search: searchParams.toString()
+        });
     }
 
     componentWillUnmount() {
@@ -40,12 +62,13 @@ class MapFilters extends Component {
 
     async handleDayChange(ev) {
         ev.preventDefault();
-        const day = ev.target.attributes['alt'].value;
+        const day = ev.target.attributes['alt'] ? ev.target.attributes['alt'].value : 'dilluns';
         let dayOfWeek = this.state.dayOfWeekNames.findIndex((name) => name === day);
         if (dayOfWeek === -1) {
             dayOfWeek = 0;
         }
         await this.setState({ dayOfWeek: dayOfWeek + 1 });
+        this.addQuery('dow', dayOfWeek + 1);
         this.props.onFiltersChange({ radius: this.state.radius, dayOfWeek: this.state.dayOfWeek });
     }
 
@@ -67,7 +90,7 @@ class MapFilters extends Component {
         return (
             <div className="map-filters">
                 <div className="map-filter-radius">
-                    <input type="range" min="1000" max="5000" step="250" list="ranges" onChange={this.handleRadiusChange.bind(this)} />
+                    <input type="range" min="1000" max="5000" step="250" list="ranges" value={this.state.radius} onChange={this.handleRadiusChange.bind(this)} />
                     <datalist id="ranges">
                         <option label="1000" value="1000" />
                         <option label={`Distància: ${this.state.radius}`} value={`Distància: ${this.state.radius}`} />
